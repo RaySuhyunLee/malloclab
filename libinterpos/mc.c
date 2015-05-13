47af8f46b04ea2cdc66e44a30ab2804ec7f049dc
Suhyun Lee <raysuhyunlee@gmail.com>
2015-05-13 20:01:23 +0900

    Migration

---
+++ libinterpos/mc.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdlib.h>
+
+void *malloc(size_t size) {
+	static void *(*mallocp)(size_t);
+	char *error;
+	void *ptr;
+
+	if (!mallocp) {
+		mallocp = dlsym(RTLD_NEXT, "malloc");
+		if ((error = dlerror()) != NULL) {
+			fputs(error, stderr);
+			exit(1);
+		}
+	}
+	ptr = mallocp(size);
+	printf("malloc(%d) = %p\n", (int)size, ptr);
+	return ptr;
+}
+
+void free(void *ptr)
+{
+	static void (*freep)(void *);
+	char *error;
+
+	if (!freep) {
+		freep = dlsym(RTLD_NEXT, "free");
+		if ((error = dlerror()) != NULL) {
+			fputs(error, stderr);
+			exit(1);
+		}
+	}
+	printf("free(%p)\n", ptr);
+	freep(ptr);
+}
---
+++ libinterpos/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int *p1, *p2;
+
+	p1 = malloc(10);
+	p2 = malloc(20);
+	p1[0] = 10;
+	p2[0] = 20;
+
+	printf("p1[0]: %d\n", p1[0]);
+	printf("p2[0]: %d\n", p2[0]);
+
+	free(p1);
+	free(p2);
+}
